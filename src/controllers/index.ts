5a8ddebfa399704b7ce2c7604c97f72824f7a849
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-17 00:12:46 +0300

    add io.on connection controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,113 @@
+import { Socket } from "socket.io";
+import { io, SocketList } from "../server";
+
+export const disconnect = (socket: Socket) => {
+  socket.on("disconnect", () => {
+    try {
+      socket.disconnect();
+      console.log("User disconnected!");
+    } catch (err) {
+      console.log("Error in disconnect: ", err);
+    }
+  });
+};
+
+export const checkUser = (socket: Socket, socketList: SocketList) => {
+  socket.on("BE-join-room", async ({ roomId, userName }) => {
+    // Socket Join RoomName
+    socket.join(roomId);
+    socketList[socket.id] = { userName, video: true, audio: true };
+
+    // Set User List
+    try {
+      const roomSockets = await io.of("/").in(roomId).fetchSockets();
+      const users: Array<{
+        userId: string;
+        info: { userName: string; video: boolean; audio: boolean };
+      }> = [];
+
+      for (const client of roomSockets) {
+        const socketInfo = socketList[client.id];
+        if (socketInfo) {
+          users.push({ userId: client.id, info: socketInfo });
+        }
+      }
+
+      socket.to(roomId).emit("FE-user-join", users);
+    } catch (error) {
+      io.in(roomId).emit("FE-error-user-exist", { err: true });
+    }
+  });
+};
+
+export const joinRoom = (socket: Socket, socketList: SocketList) => {
+  socket.on("BE-join-room", async ({ roomId, userName }) => {
+    // Socket Join RoomName
+    socket.join(roomId);
+    socketList[socket.id] = { userName, video: true, audio: true };
+
+    // Set User List
+    try {
+      const roomSockets = await io.of("/").in(roomId).fetchSockets();
+      const users: Array<{
+        userId: string;
+        info: { userName: string; video: boolean; audio: boolean };
+      }> = [];
+
+      for (const client of roomSockets) {
+        const socketInfo = socketList[client.id];
+        if (socketInfo) {
+          users.push({ userId: client.id, info: socketInfo });
+        }
+      }
+
+      socket.to(roomId).emit("FE-user-join", users);
+    } catch (error) {
+      io.in(roomId).emit("FE-error-user-exist", { err: true });
+    }
+  });
+};
+
+export const callUser = (socket: Socket, socketList: SocketList) => {
+  socket.on("BE-call-user", ({ userToCall, from, signal }) => {
+    io.to(userToCall).emit("FE-receive-call", {
+      signal,
+      from,
+      info: socketList[socket.id],
+    });
+  });
+};
+
+export const acceptCall = (socket: Socket) => {
+  socket.on("BE-accept-call", ({ signal, to }) => {
+    io.to(to).emit("FE-call-accepted", {
+      signal,
+      answerId: socket.id,
+    });
+  });
+};
+
+export const sendMessage = (socket: Socket) => {
+  socket.on("BE-send-message", ({ roomId, msg, sender }) => {
+    io.in(roomId).emit("FE-receive-message", { msg, sender });
+  });
+};
+
+export const leaveRoom = (socket: Socket, socketList: SocketList) => {
+  socket.on("BE-leave-room", ({ roomId, leaver }) => {
+    delete socketList[socket.id];
+    socket.to(roomId).emit("FE-user-leave", { userId: socket.id, userName: [socket.id] });
+    socket.leave(roomId);
+  });
+};
+
+export const toggleCameraAudio = (socket: Socket, socketList: SocketList) => {
+  socket.on("BE-toggle-camera-audio", ({ roomId, switchTarget }) => {
+    if (switchTarget === "video") {
+      socketList[socket.id].video = !socketList[socket.id].video;
+    } else {
+      socketList[socket.id].audio = !socketList[socket.id].audio;
+    }
+    socket.to(roomId).emit("FE-toggle-camera", { userId: socket.id, switchTarget });
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
-import express from "express";
-import { createServer } from "http";
+import express, { Request, Response } from "express";
+import { createServer, Server as HttpServer } from "http";
 import { Server, Socket } from "socket.io";
 import path from "path";
+import * as controllers from "./controllers";
+
+export interface SocketList {
+  [key: string]: { userName: string; video: boolean; audio: boolean };
+}
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
+const httpServer: HttpServer = createServer(app);
+export const io: Server = new Server(httpServer);
 
-io.on("connection", (socket: Socket) => {});
+const socketList: SocketList = {};
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 
-const PORT = process.env.PORT || 3001;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running.");
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log(`New User connected: ${socket.id}`);
+
+  controllers.checkUser(socket, socketList);
+  controllers.joinRoom(socket, socketList);
+  controllers.callUser(socket, socketList);
+  controllers.acceptCall(socket);
+  controllers.sendMessage(socket);
+  controllers.leaveRoom(socket, socketList);
+  controllers.toggleCameraAudio(socket, socketList);
+  controllers.disconnect(socket);
+});
 
 httpServer.listen(PORT, () => {
-  console.log(`Connected : ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
