7d4e0bfe162d0227796c85045e1ea74075f79e0e
serhii.ryabko <serhii.ryabko@gmail.com>
2024-12-02 19:50:21 +0200

    update corsOptions

--- src/peer-server.ts
+++ src/peer-server.ts
@@ -1,3 +1,12 @@
 import { PeerServer } from "peer";
 
-PeerServer({ port: 443, path: "/", corsOptions: { origin: "*", credentials: true } });
+PeerServer({
+  port: 443,
+  path: "/",
+  corsOptions: {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  },
+});
